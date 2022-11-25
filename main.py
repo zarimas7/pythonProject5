19511779f619bf187234e65a7c7ffcc69522c3fc
zarimas7 <zarimas28@gmail.com>
2022-11-25 14:30:02 +0600

    some changes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import asyncio
+
 from aiogram.utils import executor
 from config import dp
 import logging
-from handlers import client, callback, extra, admin, fsm_anketa
+from handlers import client, callback, extra, admin, fsm_anketa, notification
 from database.bot_db import sql_create
 
 async def on_startup(_):
+    asyncio.create_task(notification.scheduler())
     sql_create()
 
 
@@ -12,7 +15,7 @@ client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 fsm_anketa.register_handlers_fsm_anketa(dp)
-extra.register_handlers_extra(dp)
+notification.register_handlers_notification(dp)
 
 extra.register_handlers_extra(dp)
 
